5f749bbfd607380d2417d1fe031593f9d0a85ccd
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-28 15:01:09 +1200

    Extend Sync#load_config to read ERB within sync.yml

--- lib/sync.rb
+++ lib/sync.rb
@@ -25,7 +25,7 @@ module Sync
     # Loads the configuration from a given YAML file and environment (such as production)
     def load_config(filename, environment)
       reset_config
-      yaml = YAML.load_file(filename)[environment.to_s]
+      yaml = YAML.load(ERB.new(File.read(filename)).result)[environment.to_s]
       raise ArgumentError, "The #{environment} environment does not exist in #{filename}" if yaml.nil?
       yaml.each{|key, value| config[key.to_sym] = value }
       setup_client
