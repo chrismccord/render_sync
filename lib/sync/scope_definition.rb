bba221d43513cf81c466a44d8c22c1dd790b6c9f
wursttheke <philipp@caution-media.de>
2014-02-26 09:32:19 +0100

    move param check into seperate method

--- lib/sync/scope_definition.rb
+++ lib/sync/scope_definition.rb
@@ -3,14 +3,27 @@ module Sync
     attr_accessor :klass, :name, :lambda, :parameters, :args
     
     def initialize(klass, name, lambda)
-      unless (invalid = lambda.parameters.map { |p| p[1] } - klass.column_names.map(&:to_sym) - klass.instance_methods) == []
-        raise ArgumentError, "Invalid parameters #{invalid}. Parameter names of the sync_scope lambda definition may only contain ActiveRecord column names or instance methods of #{klass.name}."
-      end
-
+      self.class.param_names_valid?(klass, lambda)
+      
       @klass = klass
       @name = name
       @lambda = lambda
       @parameters = lambda.parameters.map { |p| p[1] }
+    end
+    
+    # Checks the validity of the parameter names contained in the lambda definition.
+    # E.g. if the lambda looks like this:
+    #
+    # ->(user) { where(user_id: user.id) }
+    #
+    # The name of the passed argument (user) must be present as a column name or an
+    # instance method (e.g. an association) of the ActiveRecord object.
+    #
+    def self.param_names_valid?(klass, lambda)
+      unless (invalid = lambda.parameters.map { |p| p[1] } - klass.column_names.map(&:to_sym) - klass.instance_methods) == []
+        raise ArgumentError, "Invalid parameters #{invalid}. Parameter names of the sync_scope lambda definition may only contain ActiveRecord column names or instance methods of #{klass.name}."
+      end
+      true
     end
    
   end
